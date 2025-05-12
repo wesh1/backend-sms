a1995de3e907963176ef44fa8f711ecee01b42e1
William Salazar <wwwwesh@gmail.com>
2025-05-12 00:00:32 -0600

    Initial project structure

---
+++ src/main/java/com/wesh/backend_sms/BackendSmsApplication.java
@@ -0,0 +1,13 @@
+package com.wesh.backend_sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesh/backend_sms/BackendSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesh.backend_sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
